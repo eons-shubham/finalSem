8f37a4330774484be74b0215bb9e418cb5e053bd
shubham <eons.shubham@gmail.com>
2023-05-18 18:14:57 +0530

    fs

---
+++ driver/src/pages/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./home.css"
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Widget from '../../components/widgets/Widget'
+import Table from '../../components/Table/Table'
+const Home = () => {
+  return (
+    <div className="home">
+       <Sidebar/>
+       <div className="homeContainer">
+        <Navbar/>
+        
+        <div className="listContainer">
+          <div className="listTitle1">
+              Driver Request
+          </div>
+          <Table/>
+        
+          <div className="listTitle2">
+              Driver Details
+          </div>
+          <Table/>
+        </div>
+       </div>
+    </div>
+   
+  )
+}
+
+export default Home
---
+++ driver/src/pages/bus/Bus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './bus.css'
+const Bus = () => {
+  return (
+    <div className='bus'>Bus</div>
+  )
+}
+
+export default Bus
