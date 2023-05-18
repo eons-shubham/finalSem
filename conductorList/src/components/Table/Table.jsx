ba5fbd68ed52dd0831682a17b34c6c2ef01a8589
shubham <eons.shubham@gmail.com>
2023-05-18 18:19:18 +0530

    i

---
+++ conductorList/src/components/Table/Table.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './table.css'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+const List = () => {
+    const rows = [
+        {
+          id: 1143155,
+          Driver: "Manish ji",
+        },
+        {
+          id: 2235235,
+          Driver: "Dubey ji",
+        },
+        {
+          id: 2342353,
+          Driver: "Joshi ji",
+        },
+        {
+          id: 2357741,
+          Driver: "Tiwari ji",
+        },
+        {
+          id: 2342355,
+          Driver: "Bisht ji",
+        },
+      ];
+  return ( <TableContainer component={Paper} className="table">
+  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableHead>
+      <TableRow>
+      <TableCell className="tableCell">First Name</TableCell>
+      <TableCell className="tableCell">Last Name</TableCell>
+      <TableCell className="tableCell">Contact No.</TableCell>
+      <TableCell className="tableCell">Make Coordinator</TableCell>
+            
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {rows.map((row) => (
+        <TableRow
+          key={row.id}
+        >
+          <TableCell component="th" scope="row">
+            {row.id}
+          </TableCell>
+          <TableCell className="tableCell">{row.fname}</TableCell>
+          <TableCell className="tableCell">{row.lname}</TableCell>
+          <TableCell className="tableCell">{row.contactno}</TableCell>
+          <TableCell className="tableCell">{row.coordinator}</TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+</TableContainer>
+  )
+}
+
+export default List
---
+++ conductorList/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import './sidebae.css'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import LogoutIcon from '@mui/icons-material/Logout';
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="top"><span className="logo">Conductor List</span></div>
+        <hr/>
+        <div className="center">
+            <ul>
+                <li>
+                    <DashboardIcon className="icon"/>
+                    <span>
+                        Dashboard
+                    </span>
+                </li>
+                
+                <li>
+                    <DirectionsBusIcon className="icon"/>
+                    <span>
+                       Bus Request
+                    </span>
+                </li>
+                <li>
+                    <CurrencyRupeeIcon className="icon"/>
+                    <span>
+                        Fees
+                    </span>
+                </li>
+                <li>
+                    <PersonPinIcon className="icon"/>
+                    <span>
+                        Student
+                    </span>
+                </li>
+                <li>
+                    <PersonPinIcon className="icon"/>
+                    <span>
+                        Driver
+                    </span>
+                </li>
+                <li>
+                    <PersonPinIcon className="icon"/>
+                    <span>
+                        Conductor
+                    </span>
+                </li>
+                <li>
+                    <PersonPinIcon className="icon"/>
+                    <span>
+                        Co-ordinator
+                    </span>
+                </li>
+                <li>
+                    <LogoutIcon className="icon"/>
+                    <span>
+                        Logout
+                    </span>
+                </li>
+                
+            </ul>
+        </div>
+        <div className="bottom">
+           <div className="colorOption"></div>
+           <div className="colorOption"></div>
+            </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
